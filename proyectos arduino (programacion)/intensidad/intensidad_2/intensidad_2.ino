50f01d11a2bb8890c40866202dcdcef035caa716
piaglezm <piaglezm@gmail.com>
2021-06-24 19:41:26 -0500

    Revert "hihihi"
    This reverts commit 320b7bd6df365c17d14becdd87c849bf344faf7a.

--- proyectos arduino (programacion)/intensidad/intensidad_2/intensidad_2.ino
+++ proyectos arduino (programacion)/intensidad/intensidad_2/intensidad_2.ino
@@ -6,5 +6,14 @@ pinMode(5,OUTPUT);
 }
 
 void loop() {
-
+  // put your main code here, to run repeatedly:
+analogWrite(5,brillo);
+delay(200);
+ if (brillo==0){
+  delay(1000);
+  brillo=60;
+ }
+else{
+  brillo = brillo - 10;
+ }
 }

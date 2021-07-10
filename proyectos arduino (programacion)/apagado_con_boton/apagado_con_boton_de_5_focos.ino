6dcc221276906758ad245f3fe8983a6c5f394df9
piaglezm <piaglezm@gmail.com>
2021-07-10 12:39:48 -0500

    hice un nuevo proyecto

---
+++ proyectos arduino (programacion)/apagado_con_boton/apagado_con_boton_de_5_focos.ino
@@ -0,0 +1,30 @@
+int azul = 4;
+int rojo = 2;
+int verde = 7;
+int amarillo = 13;
+int azul2 = 12;
+int boton = 3;
+void setup() {
+pinMode(azul,OUTPUT);
+pinMode(rojo,OUTPUT);
+pinMode(verde,OUTPUT);
+pinMode(amarillo,OUTPUT);
+pinMode(azul2,OUTPUT);
+pinMode(boton,INPUT);
+}
+void loop() {
+if (digitalRead(boton)==LOW){
+digitalWrite(azul,HIGH);
+digitalWrite(verde,LOW);
+digitalWrite(rojo,HIGH);
+digitalWrite(amarillo,LOW);
+digitalWrite(azul2,HIGH);
+}
+ else if (digitalRead(boton)==HIGH){
+digitalWrite(azul,LOW);
+digitalWrite(verde,HIGH);
+digitalWrite(rojo,LOW);
+digitalWrite(amarillo,HIGH);
+digitalWrite(azul2,LOW);
+ }
+}

3c8ca9a7b73ce6b9b58f1c8c6b982fcafa1e6b9e
Eduardo Gonzalez <fectda@gmail.com>
2021-07-24 14:22:15 -0500

    test sounds an button

--- eduardo/tone/tone.ino
+++ eduardo/tone/tone.ino
@@ -1,22 +1,35 @@
+int speaker_1 = 8;
+int speaker_2 = 12;
+int button = 13;
+
 void setup() {
-  pinMode(8,OUTPUT);
+  pinMode(speaker_1,OUTPUT);
+  pinMode(speaker_2,OUTPUT);
+  pinMode(speaker_2,INPUT);
 }
 
 void loop() {
-  tone(8,262,250);
+  if(digitalRead(button) == HIGH) {
+      sound(speaker_1);
+    sound(speaker_2);
+  }
+}
+
+void sound(int speaker ) {
+  tone(speaker,262,25);
   delay(325);
-  tone(8,294,250);
+  tone(speaker,294,25);
   delay(325);
-  tone(8,330,250);
+  tone(speaker,330,25);
   delay(325);
-  tone(8,349,250);
+  tone(speaker,349,25);
   delay(325);
-  tone(8,392,250);
+  tone(speaker,392,25);
   delay(325);
-  tone(8,440,250);
+  tone(speaker,440,25);
   delay(325);
-  tone(8,494,250);
+  tone(speaker,494,25);
   delay(325);
-  tone(8,523,250);
+  tone(speaker,523,25);
   delay(1000);
 }

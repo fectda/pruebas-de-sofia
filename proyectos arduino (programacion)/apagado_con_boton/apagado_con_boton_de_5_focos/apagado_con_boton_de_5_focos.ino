d04d41ba00c922cc6c811c9a3bc3e4a93fd67fb5
piaglezm <piaglezm@gmail.com>
2021-07-24 14:51:46 -0500

    complete el codigo

--- proyectos arduino (programacion)/apagado_con_boton/apagado_con_boton_de_5_focos/apagado_con_boton_de_5_focos.ino
+++ proyectos arduino (programacion)/apagado_con_boton/apagado_con_boton_de_5_focos/apagado_con_boton_de_5_focos.ino
@@ -4,6 +4,7 @@ int verde = 7;
 int amarillo = 13;
 int azul2 = 12;
 int boton = 3;
+int boton2 = 8;
 void setup() {
 pinMode(azul,OUTPUT);
 pinMode(rojo,OUTPUT);
@@ -11,20 +12,35 @@ pinMode(verde,OUTPUT);
 pinMode(amarillo,OUTPUT);
 pinMode(azul2,OUTPUT);
 pinMode(boton,INPUT);
+pinMode(boton2,INPUT);
 }
 void loop() {
 if (digitalRead(boton)==LOW){
+digitalWrite(azul,LOW);
+digitalWrite(verde,HIGH);
+digitalWrite(rojo,LOW);
+digitalWrite(amarillo,HIGH);
+digitalWrite(azul2,LOW);
+}
+ else if (digitalRead(boton)==HIGH){
+digitalWrite(azul,LOW);
+digitalWrite(verde,LOW);
+digitalWrite(rojo,LOW);
+digitalWrite(amarillo,LOW);
+digitalWrite(azul2,LOW);
+   }
+if (digitalRead(boton2)==LOW){
 digitalWrite(azul,HIGH);
 digitalWrite(verde,LOW);
 digitalWrite(rojo,HIGH);
 digitalWrite(amarillo,LOW);
 digitalWrite(azul2,HIGH);
-}
- else if (digitalRead(boton)==HIGH){
+  }
+else if (digitalRead(boton2)==HIGH){
 digitalWrite(azul,LOW);
-digitalWrite(verde,HIGH);
+digitalWrite(verde,LOW);
 digitalWrite(rojo,LOW);
-digitalWrite(amarillo,HIGH);
+digitalWrite(amarillo,LOW);
 digitalWrite(azul2,LOW);
  }
 }
